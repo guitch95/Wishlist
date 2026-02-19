1dc37fdc50a6ea8015d720b3291e47cab33fe5cb
Guillaume Richard <guitch95@gmail.com>
2026-02-19 11:25:02 +0100

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Guillaume Richard on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
